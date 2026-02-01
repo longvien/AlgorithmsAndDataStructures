837b6753dcfb2142134221589435c63d406aea20
longvien <vienngoclongvn@gmail.com>
2026-02-01 21:23:29 +0100

    adding nodes/ removing nodes form linked list

---
+++ LinkedList/src/main/java/org/mainProgramme/DoublyLinkedList.java
@@ -0,0 +1,101 @@
+package org.mainProgramme;
+
+import org.mainProgramme.Node.DoublyNode;
+
+public class DoublyLinkedList<T> {
+    public DoublyNode<T> head = null;
+    public DoublyNode<T> tail = null;
+    // add node to head
+    public void addHead(T value) {
+        DoublyNode<T> newNode = new DoublyNode<>(value);
+
+        if (head != null) {
+            head.previous = newNode;  // lastNode.previous = newNode;
+            newNode.next = head; // newNode.next = lastNode;
+            head = newNode; // head = newNode;
+        }
+        else if (head == null && tail == null) {
+            head = newNode;
+            tail = newNode;
+        }
+
+
+    }
+    // add node to tail
+    public void addTail(T value) {
+        DoublyNode<T> newNode = new DoublyNode<>(value);
+
+        if(tail != null) {
+            tail.next = newNode;
+            newNode.previous = tail;
+            tail = newNode;
+        }
+        else if (tail == null && head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+    }
+    // remove a node
+    public void remove(T value){
+        DoublyNode<T> node = head;
+        if (contains(value)) {
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    if (node.previous == null){
+                        node.next.previous = null;
+                        head = node.next;
+                        node.next = null;
+                    }
+                    else if (node.next == null){
+                        node.previous.next = null;
+                        tail = node.previous;
+                        node.previous = null;
+                    }
+                    else {
+                        node.previous.next = node.next;
+                        node.next.previous = node.previous;
+                        node.next = null;
+                        node.previous = null;
+                    }
+                }
+                node = node.next;
+            }
+        }
+        else {
+            System.out.println("Node doesn't exist!");
+        }
+    }
+
+
+    public void returnValueHeadToTail() {
+        DoublyNode<T> current = head;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
+
+    public void returnValueTailToHead() {
+        DoublyNode<T> current = tail;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.previous;
+        }
+    }
+
+    // find value and return node that holds value
+    public DoublyNode<T> find(T value) {
+        DoublyNode<T> counter = head;
+        while(counter != null) {
+            if (counter.value.equals(value)) {
+                return counter;
+            }
+            counter = counter.next;
+        }
+    return null;
+    }
+    // return true / false if the value is in the list
+    public boolean contains(T value) {
+        return find(value) != null;
+    }
+}
--- LinkedList/src/main/java/org/mainProgramme/Main.java
+++ LinkedList/src/main/java/org/mainProgramme/Main.java
@@ -5,27 +5,44 @@ import org.mainProgramme.Node.DoublyNode;
 
 public class Main {
     public static void main(String[] args) {
+        // Create single nodes
         SinglyNode<Integer> singlyNode1 = new SinglyNode<>(1);
         SinglyNode<Integer> singlyNode2 = new SinglyNode<>(2);
         SinglyNode<Integer> singlyNode3 = new SinglyNode<>(3);
-
+        // [1] -> [2] -> [3]
+        // Set next for single nodes
         singlyNode1.next = singlyNode2;
         singlyNode2.next = singlyNode3;
 
+        // Print out all single nodes
         while (singlyNode1 != null) {
             System.out.println(singlyNode1.getValue());
             singlyNode1 = singlyNode1.next;
         }
-
+        // Create a doubly node
         DoublyNode<Float> doublyNode1 = new DoublyNode<>(1.0f);
         DoublyNode<Float> doublyNode2 = new DoublyNode<>(2.0f);
         DoublyNode<Float> doublyNode3 = new DoublyNode<>(3.0f);
-
+        // [1] <-> [2] <-> [3]
         doublyNode1.next = doublyNode2;
         doublyNode2.previous = doublyNode1;
 
         doublyNode2.next = doublyNode3;
-        doublyNode3.previous = doublyNode2; //
+        doublyNode3.previous = doublyNode2;
+
+        // using LinkedList
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
+        dll.addHead(100);
+        dll.addHead(200);
+        dll.addHead(300);
+        dll.returnValueHeadToTail();
+        dll.addTail(50);
+        dll.remove(300);
+        dll.returnValueTailToHead();
+
+        // head -> [300] <-> ([200] <-> is removed) [100] <-> [50] <- tail
+        System.out.println(dll.contains(200));
+
     }
 
 
