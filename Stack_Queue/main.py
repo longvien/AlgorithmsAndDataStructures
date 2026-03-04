9e80dd7875435bcf8ff57b77b6e54ae1f7c9f5c1
longvien <vienngoclongvn@gmail.com>
2026-03-04 06:21:55 +0100

    implement queue

---
+++ Stack_Queue/main.py
@@ -0,0 +1,9 @@
+from queue.qUeUe import Queue
+
+myQueue = Queue()
+myQueue.enqueue(5)
+myQueue.enqueue(4)
+myQueue.enqueue(3)
+myQueue.enqueue(2)
+myQueue.enqueue(1)
+print(myQueue.peek())
---
+++ Stack_Queue/queue/qUeUe.py
@@ -0,0 +1,26 @@
+from queueNode import QueueNode
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def enqueue(self, value):
+        newNode = QueueNode(value)
+        if self.head is None and self.tail is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+    def dequeue(self):
+        if self.head is not None:
+            current = self.head
+            self.head = self.head.next
+            current.next = None
+        else:
+            self.tail = None
+
+    def peek(self):
+        return self.head.value
