f46a3736edc0d69974bd0b588195f625a4fb0f12
longvien <vienngoclongvn@gmail.com>
2026-03-06 06:12:49 +0100

    Stack Implementation

--- Stack_Queue/main.py
+++ Stack_Queue/main.py
@@ -1,4 +1,5 @@
 from queue import Queue
+from stack import Stack
 def main():
     myQueue = Queue()
     myQueue.enqueue(5)
@@ -9,5 +10,15 @@ def main():
     for i in range(5):
         myQueue.dequeue()
     print(myQueue.peek())
+    myStack = Stack()
+    myStack.push(1)
+    myStack.push(2)
+    myStack.push(3)
+    myStack.push(4)
+    myStack.push(5)
+    for i in range(1):
+        myStack.pop()
+    print(myStack.peek())
+
 if __name__ == "__main__":
     main()  #execute when file is run directly, not when imported ad a module
