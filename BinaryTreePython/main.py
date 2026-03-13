0f48cfb28a73b115ca1c59cbf97d6732a0059b09
longvien <vienngoclongvn@gmail.com>
2026-03-13 15:12:13 +0100

    implement add function successfully

--- BinaryTreePython/main.py
+++ BinaryTreePython/main.py
@@ -1,11 +1,11 @@
 from BinaryNode import *
 from BinarySearchTree import *
 #basic tree
-#node1 = BinaryNode("root")
-#node2 = BinaryNode("left")
-#node3 = BinaryNode("right")
-#node1.left = node2
-#node1.right = node3
+node1 = BinaryNode("root")
+node2 = BinaryNode("left")
+node3 = BinaryNode("right")
+node1.left = node2
+node1.right = node3
 
 BST = BinarySearchTree()
 BST.add(2)
@@ -14,5 +14,5 @@ BST.add(4)
 BST.add(1)
 BST.add(1.5)
 BST.preOrderTraversal(print) # current -> left -> right | Pre-order Traversal
-#BST.inOrderTraversal(print) # left -> current -> right | In-order Traversal (Sorted)
-#BST.postOrderTraversal(print) # left -> right -> current | Post-order Traversal
+BST.inOrderTraversal(print) # left -> current -> right | In-order Traversal (Sorted)
+BST.postOrderTraversal(print) # left -> right -> current | Post-order Traversal
