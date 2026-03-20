dd904658ebca7df8099347bc4d7d6042a5b8c49d
longvien <vienngoclongvn@gmail.com>
2026-03-20 22:38:17 +0100

    attempted to implement DFS for graph usage

--- Graph/graphClass.py
+++ Graph/graphClass.py
@@ -1,6 +1,8 @@
+from Stack import Stack
 class GraphClass:
     def __init__(self):
         self.adj = {}
+        self.tarray = []
     def addEdge(self, a, b):
         if a not in self.adj:
             self.adj[a] = []
@@ -8,5 +10,21 @@ class GraphClass:
             self.adj[b] = []
         self.adj[a].append(b)
         self.adj[b].append(a)
+
     def returnGraph(self):
         return self.adj
+
+    def depthFirstSearch(self):
+        for i in self.adj:
+            self.tarray.append(i)
+        self._depthFirstSearch(self.tarray[0], self.tarray)
+    def _depthFirstSearch(self, n, array):
+        stack = Stack()
+        stack.push(n)
+        stack.pop(n)
+        for i in
+
+
+
+
+
