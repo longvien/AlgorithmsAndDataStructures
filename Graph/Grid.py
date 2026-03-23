2735e548ea3224736580260165dc93525a2d8a9d
longvien <vienngoclongvn@gmail.com>
2026-03-23 05:56:34 +0100

    complete implementing gridComponentsCounter, GridClass

--- Graph/Grid.py
+++ Graph/Grid.py
@@ -1,45 +1,34 @@
-grid = [[1, 0, 1],
-        [0, 1, 0],
-        [1, 1, 1]
-        ]
+class Grid:
+    def __init__(self):
+        self.visited = set()
 
-gridADJ = {
-    (0,0): [(0,1)],
-    (0,1): [(0,0), (1,1)],
-    (1,1): [(0,1)],
-    (2,0): [],
-    (2,2): []
-}
-
-visited = set()
-def componentCounterGrid():
-    counter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1 and (r, c) not in visited:
-                counter += 1
-                DFSGrid(r, c)
-    return counter
+    def componentCounterGrid(self, grid):
+        counter = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and (r, c) not in self.visited:
+                    counter += 1
+                    self.DFSGrid(r, c, grid)
+        return counter
 
 
-def DFSGrid(r, c):
-    rows = len(grid)
-    cols = len(grid[0])
-    if 0 > r or r >= rows or  0 > c or c >= cols:
-        return
-    if (r, c) in visited:
-        return
-    if grid[r][c] == 0:
-        return
-    if grid[r][c] == 1:
-        visited.add((r, c))
-        DFSGrid(r + 1, c)
-        DFSGrid(r - 1, c)
-        DFSGrid(r, c + 1)
-        DFSGrid(r, c - 1)
-    return visited
+    def DFSGrid(self, r, c, grid):
+        rows = len(grid)
+        cols = len(grid[0])
+        if 0 > r or r >= rows or  0 > c or c >= cols:
+            return
+        if (r, c) in self.visited:
+            return
+        if grid[r][c] == 0:
+            return
+        if grid[r][c] == 1:
+            self.visited.add((r, c))
+            self.DFSGrid(r + 1, c, grid)
+            self.DFSGrid(r - 1, c, grid)
+            self.DFSGrid(r, c + 1, grid)
+            self.DFSGrid(r, c - 1, grid)
+        return self.visited
 
-print(componentCounterGrid())
 
--- Graph/main.py
+++ Graph/main.py
@@ -1,11 +1,31 @@
 from graphClass import GraphClass
-
+from Grid import Grid
 myGraph = GraphClass()
 myGraph.addEdge('A', 'B')
 myGraph.addEdge('A', 'C')
 myGraph.addEdge('B', 'D')
 myGraph.addEdge('D', 'C')
 myGraph.addEdge('F', 'E')
+print('DFS Interactive')
 for i in myGraph.DFSInterative():
     print(i)
+print('Connected Components Counter Graph')
 print(myGraph.componentsCounter())
+
+#examoleGrid
+grid = [[1, 0, 1],
+        [0, 1, 0],
+        [1, 1, 1]
+        ]
+#exampleGridInADJ
+gridADJ = {
+    (0,0): [(0,1)],
+    (0,1): [(0,0), (1,1)],
+    (1,1): [(0,1)],
+    (2,0): [],
+    (2,2): []
+}
+
+myGrid = Grid()
+print('Connected Components Counter Grid')
+print(myGrid.componentCounterGrid(grid))
