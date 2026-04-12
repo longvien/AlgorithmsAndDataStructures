7df26067b2dccbb54b8b515d581315b7b6944590
longvien <vienngoclongvn@gmail.com>
2026-04-12 21:45:59 +0200

    implement Kruskal, DSU using parent array

--- DisjointSetUnion/DSUN.py
+++ DisjointSetUnion/DSUN.py
@@ -13,5 +13,20 @@ class DSUN:
         rootY = self.find(y)
         if rootX != rootY:
             self.parentA[rootX] = rootY
-
-
+    def getWeight(self, edge):
+        return edge[2]
+    def KruskalAlgorithm(self, graph):
+        edges = []
+        mst = []
+        for start, des, cost in graph:
+            if self.parentA[start] != start:
+                self.makeSet(start)
+            if self.parentA[des] != des:
+                self.makeSet(des)
+            edges.append((start, des, cost))
+        edges.sort(key=self.getWeight)
+        for edge in edges:
+            if self.find(edge[0]) != self.find(edge[1]):
+                self.union(edge[0], edge[1])
+                mst.append(edge)
+        return mst
--- DisjointSetUnion/main.py
+++ DisjointSetUnion/main.py
@@ -1,4 +1,10 @@
 from DSU import DSU
+from DSUN import DSUN
 myDSU = DSU()
 graph = [('A', 'B', 1), ('B', 'C', 4), ('C', 'A', 3)]
 print(myDSU.KruskalAlgorithm(graph))
+
+myDSUN = DSUN(4)
+graph = [(1, 2, 1), (2, 3, 5), (3, 1, 2)]
+print(myDSUN.KruskalAlgorithm(graph))
+
