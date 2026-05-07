216bef20fb26ba56a9ce7aba80545c786c847991
longvien <vienngoclongvn@gmail.com>
2026-05-07 22:57:56 +0200

    add memoization version

--- DynamicProgramming/CoinChange.py
+++ DynamicProgramming/CoinChange.py
@@ -1,20 +1,36 @@
-def coinChange(cost, amount):
+#memoization
+def coinChangeM(cost, amount):
     dp = [float("inf") for i in range(amount + 1)]
     dp[0] = 0
-    result =  _coinChange(cost, amount, dp)
+    result =  _coinChangeM(cost, amount, dp)
     if result == float("inf"):
         return -1
     return result
-def _coinChange(cost, amount, dp):
+def _coinChangeM(cost, amount, dp):
     if amount == 0:
         return 0
     if dp[amount] != float("inf"):
         return dp[amount]
     for c in cost:
         if c <= amount:
-            dp[amount] = min(dp[amount], _coinChange(cost, amount - c, dp) + 1)
+            dp[amount] = min(dp[amount], _coinChangeM(cost, amount - c, dp) + 1)
     return dp[amount]
 
-cost = [186,419,83,408]
-print(coinChange(cost, 6249))
+costM = [1, 3, 5]
+print(coinChangeM(costM, 11))
 
+#tabulation
+
+def coinChangeT(cost, amount):
+    dp = [float("inf") for i in range(amount + 1)]
+    dp[0] = 0
+    for i in range(1, amount + 1):
+        for c in cost:
+            if c <= i:
+                dp[i] = min(dp[i], dp[i - c] + 1)
+    if dp[amount] == float("inf"):
+        return -1
+    return dp[amount]
+
+costT = [2]
+print(coinChangeT(costT, 3))
--- DynamicProgramming/HouseRobber.py
+++ DynamicProgramming/HouseRobber.py
@@ -1,12 +1,32 @@
-class Solution:
-    def rob(self, nums: list[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        dp = [0 for i in range(len(nums))]
-        dp[0] = nums[0]
-        dp[1] = max(dp[0], nums[1])
-        if len(nums) == 2:
-            return dp[1]
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-        return dp[len(nums) - 1]
+#memoization
+def HouseRobberM(houses):
+    if len(houses) == 1:
+        return houses[0]
+    dp = [-1 for i in range(len(houses))]
+    dp[0] = houses[0]
+    dp[1] = max(dp[0], houses[1])
+    return _HouseRobberM(houses, dp, len(houses) - 1)
+
+def _HouseRobberM(house, dp, n):
+    if dp[n] != -1:
+        return dp[n]
+    dp[n] = max(_HouseRobberM(house, dp, n - 1), _HouseRobberM(house, dp, n - 2) + house[n])
+    return dp[n]
+
+houses = [2,7,9,3,1]
+print(HouseRobberM(houses))
+
+#tabulation
+def HouseRobberT(houses):
+    if len(houses) == 1:
+        return houses[0]
+    dp = [-1 for i in range(len(houses))]
+    dp[0] = houses[0]
+    dp[1] = max(dp[0], houses[1])
+    if len(houses) == 2:
+        return dp[1]
+    for i in range(2, len(houses)):
+        dp[i] = max(dp[i - 1], dp[i - 2] + houses[i])
+    return dp[len(houses) - 1]
+
+print(HouseRobberT([2,7,9,3,1]))
