73181dbe9b8a8adc43bbeb69cb7b2a61874d34b9
longvien <vienngoclongvn@gmail.com>
2026-05-09 21:42:43 +0200

    LIS

--- DynamicProgramming/LIS.py
+++ DynamicProgramming/LIS.py
@@ -1,11 +1,16 @@
-#LIS | Largest Increasing Subsequence
-def LIS(nums) -> int:
-    dp = [1 for i in range(len(nums) + 1)]
-    if len(nums) == 1:
-        return 1
-    return _LIS(nums, dp, len(nums) - 1)
-def _LIS(nums, dp, n):
-    if n == 0:
-        return 1
-    if dp[n] != 1:
-        return dp[n]
+# #LIS | Largest Increasing Subsequence
+#tabulation
+def LIST(nums):
+    dp = [1 for i in range(len(nums))]
+    for i in range(1, len(nums)):
+        for n in range(i - 1, -1, -1):
+            if nums[n] < nums[i]:
+                dp[i] = max(dp[i], dp[n] + 1)
+    print(dp)
+    maxL = max(dp)
+    return maxL
+
+arr = [1, 2, 5, 6, 7, 5, 4, 4]
+print(LIST(arr)) # Pass! Time Complexity: 0(n**2)
+
+
