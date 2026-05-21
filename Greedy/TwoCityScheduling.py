6074b4583a1bf2454dcd5cb2e2e02a158fbd92d4
longvien <vienngoclongvn@gmail.com>
2026-05-21 22:32:26 +0200

    Greedy Practice, LC1029 Two City Scheduling

---
+++ Greedy/TwoCityScheduling.py
@@ -0,0 +1,33 @@
+class Solution:
+    def twoCitySchedCost(self, costs: list[list[int]]) -> int:
+        l = len(costs)
+        a = l // 2
+        b = l // 2
+        n = l - 1
+        total = 0
+        limit = False
+        costs.sort(key=self.sortDifference)
+        while n >= 0:
+            if costs[n][0] > costs[n][1]:
+                total += costs[n][1]
+                b -= 1
+            else:
+                total += costs[n][0]
+                a -= 1
+            n -= 1
+            if a == 0 or b == 0:
+                limit = True
+                break
+        if limit:
+            if a > b:
+                for i in range(n, -1, -1):
+                    total += costs[i][0]
+            else:
+                for i in range(n, -1, -1):
+                    total += costs[i][1]
+        return total
+
+    def sortDifference(self, pair):
+        return abs(pair[0] - pair[1])
+
+#leetcode, task 1029
