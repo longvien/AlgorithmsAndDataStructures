1c8d24dfdc2034286f1f598f21ce59affaee2aa8
longvien <vienngoclongvn@gmail.com>
2026-06-16 22:02:15 +0200

    ILP Implementation

--- DynamicProgramming/DeleteAndEarn.py
+++ DynamicProgramming/DeleteAndEarn.py
@@ -1,44 +1,28 @@
-# class Solution:
-#     def deleteAndEarn(self, nums: List[int]) -> int:
-#         if len(nums) == 1: return nums[0]
-#         napr = {}
-#         for n in nums:
-#             if n not in napr:
-#                 napr[n] = 0
-#             napr[n] += n
-#         rob = []
-#         for i in napr:
-#             rob.append((i, napr[i]))
-#         rob.sort(key=self.getKey)
-#         dp = [0 for i in range(len(rob))]
-#         dp[0] = rob[0][1]
-#         if len(dp) == 1: return dp[0]
-#         if abs(rob[1][0] - rob[0][0]) > 1:
-#             dp[1] = dp[0] + rob[1][1]
-#         else:
-#             dp[1] = max(rob[0][1], rob[1][1])
-#         for i in range(2, len(rob)):
-#             if abs(rob[i][0] - rob[i - 1][0]) > 1:
-#                 dp[i] = dp[i - 1] + rob[i][1]
-#             else:
-#                 dp[i] = max(dp[i - 1], dp[i - 2] + rob[i][1])
-#         return dp[len(dp) - 1]
-#     def getKey(self, n):
-#         return n[0]
-#
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if len(nums) == 1: return nums[0]
+        napr = {}
+        for n in nums:
+            if n not in napr:
+                napr[n] = 0
+            napr[n] += n
+        rob = []
+        for i in napr:
+            rob.append((i, napr[i]))
+        rob.sort(key=self.getKey)
+        dp = [0 for i in range(len(rob))]
+        dp[0] = rob[0][1]
+        if len(dp) == 1: return dp[0]
+        if abs(rob[1][0] - rob[0][0]) > 1:
+            dp[1] = dp[0] + rob[1][1]
+        else:
+            dp[1] = max(rob[0][1], rob[1][1])
+        for i in range(2, len(rob)):
+            if abs(rob[i][0] - rob[i - 1][0]) > 1:
+                dp[i] = dp[i - 1] + rob[i][1]
+            else:
+                dp[i] = max(dp[i - 1], dp[i - 2] + rob[i][1])
+        return dp[len(dp) - 1]
+    def getKey(self, n):
+        return n[0]
 
-from ortools.sat.python import cp_model
-
-m = cp_model.CpModel()
-
-x1 = m.NewBoolVar("x1")
-x2 = m.NewBoolVar("x2")
-x3 = m.NewBoolVar("x3")
-
-m.Add(2*x1 + 3*x2 + 5*x3 <= 5)
-m.Maximize(6*x1 + 10*x2 + 8*x3)
-
-solver = cp_model.CpSolver()
-solver.Solve(m)
-
-print(solver.Value(x1), solver.Value(x2), solver.Value(x3))
--- ILP/ilp.cpp
+++ ILP/ilp.cpp
@@ -1,6 +1,7 @@
 #include "ortools/sat/cp_model.h"
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace operations_research;
 using namespace sat;
@@ -14,20 +15,32 @@ int main () {
     vector<int> weight = {2, 3, 5};
 
     
-    BoolVar x1 = model.NewBoolVar();
-    BoolVar x2 = model.NewBoolVar();
-    BoolVar x3 = model.NewBoolVar();
+    vector<BoolVar> x;
+    for (int i = 0; i < value.size(); i ++ ){
+        x.push_back(model.NewBoolVar());
+    }
     
-    
-    model.Add(x1 * weight[0] + x2 * weight[1] + x3 * weight[2] <= limit);
+    LinearExpr weightT; 
 
+    for (int i = 0; i < x.size(); i ++) {
+        weightT += LinearExpr(x[i]) * weight[i];      // x.size() ∑ i = 1   x[i] * value[i] <= 5; 
+    } 
     
+    model.AddLessOrEqual(weightT, 5);
     
-    model.Maximize(x1 * value[0] + x2 * value[1] + x3 * value[2]);
+    LinearExpr valueT;
 
+    for (int i = 0; i < x.size(); i++) {
+        valueT += LinearExpr(x[i]) * value[i]; 
+    }
 
-    CpSolver solver;
+    model.Maximize(valueT);
+
     const CpSolverResponse response = Solve(model.Build());
-
+    cout << "Value: " << response.objective_value() << "\n";
+    cout << "Items:";
+    for (int i = 0; i < x.size(); i++) {
+        
+    }
     return 0;
 }
