30414e3eac7128412ecd545227b23518ebe8d443
longvien <vienngoclongvn@gmail.com>
2026-07-02 22:17:32 +0700

    OutOfBoundaryPath

---
+++ DynamicProgramming/2D_DP/OutOfBoundaryPaths.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<int>> dp(m+2, vector<int>(n+2,0));
+        
+    }
+};
+
+int main() {
+    Solution mySolu = Solution();
+    cout << mySolu.findPaths(2, 2, 2, 0, 0);
+    return 0;
+}
