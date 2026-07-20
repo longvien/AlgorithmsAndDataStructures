bd996f71749f69a3a39abdd323cea689d24d09b4
longvien <vienngoclongvn@gmail.com>
2026-07-20 23:01:03 +0700

    Math Phase, Sieve of Eratosthenes

---
+++ Math/SieveOfEratosthenes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3")
+
+bool SieveOfEratothenes(int n) {
+    for (int i = 2; i < (int)sqrt(n)+1; i++) {
+        if (n%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+    int x;
+    cin >> x;
+    string ans = (SieveOfEratothenes(x) == 1)? "Is Prime!" : "Is not Prime";
+    cout << ans;
+    return 0;
+}
+
+/*To check if n is a prime number, you must only iterate from 2 to √n 
+and check during the iteration if it's remainder to any num is 0, if yes => not prime num*/
