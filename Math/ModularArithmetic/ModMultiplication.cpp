0f06397c5966eae3e8ce8e0c0c8d715bfbb76dff
longvien <vienngoclongvn@gmail.com>
2026-07-31 21:53:26 +0700

    Last Day Fermat's Little, Mod Inverse

--- Math/ModularArithmetic/ModMultiplication.cpp
+++ Math/ModularArithmetic/ModMultiplication.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 #pragma GCC optimize("O3")
+typedef long long ll;
 
-ll modSubtraction(ll a, ll b, ll m) {
-    return (a%m - b%m)%m;
+ll modMultiplication(ll a, ll b, ll m) {
+    return (a%m * b%m) %m;
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    ll a, b, m;
+    ll a, b , m;
     cin >> a >> b >> m;
-    cout << modSubtraction(a,b,m) << "\n";
+    cout << modMultiplication(a, b, m) << "\n";
     return 0;
 }
