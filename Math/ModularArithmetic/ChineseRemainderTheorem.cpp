54ea52b2d0a3ebc3a355d16f3747b42ca68b9134
longvien <vienngoclongvn@gmail.com>
2026-08-07 19:18:55 +0200

    Implement Euler's Totient Function, Chinese Remainder Theorem

---
+++ Math/ModularArithmetic/ChineseRemainderTheorem.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = signed long long;
+#define mp make_pair
+#define pb push_back
+template<typename T>
+using vpair = vector<pair<T, T>>;
+#pragma GCC optimize("O3")
+
+namespace FastIO {
+	inline void init() {
+		ios_base::sync_with_stdio(false);
+		cin.tie(nullptr);
+	}
+}
+
+ll extendedEuclid(ll a, ll b, ll m) {
+    ll x1 = 1, x2 = 0, x3 = 0;
+    ll y1 = 0, y2 = 1, y3 = 0;
+    ll q = a/b;
+    ll r = a%b;
+    while (b!=1) {
+        a = b;
+        b = r;
+        x3 = x1 - q*x2;
+        y3 = y1 - q*y2;
+        x1 = x2;
+        x2 = x3;
+        y1 = y2;
+        y2 = y3;
+        q = a/b;
+        r = a%b;
+    }
+    return (x2%m+m)%m;
+}
+
+ll CRT(vpair<ll> in, ll M) {
+    ll ans = 0;
+    for (auto& [r, m] : in) {
+        ll currX = M/m;
+        currX = ((__int128)currX*(r%M))%M;
+        currX = ((__int128)currX*(extendedEuclid(M/m, m, m))%M)%M;
+        ans = (ans+currX)%M;
+    }
+    return ans;
+}
+
+signed main() {
+    FastIO::init();
+    ll t;
+    cin >> t;
+    ll productM = 1;    
+    vpair<ll> equations;
+    for (ll i = 0; i<t; i++) {
+        ll r, m;
+        cin >> r >> m;
+        if (gcd(productM, m) != 1) {
+            cout << "This is the standard crt algorithm, moduli must be co prime to each other!" << "\n";
+            return 0;
+        }
+        equations.pb({r, m});
+        productM*=m;
+    }
+    cout << "Smallest value of x which sastify the equations is: " << CRT(equations, productM) << "\n";
+    return 0;
+}
--- Math/ModularArithmetic/EulersTotientFunction.cpp
+++ Math/ModularArithmetic/EulersTotientFunction.cpp
@@ -1,12 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-//#pragma GCC optimize("O3")
+using ll = signed long long;
+#define pb push_back
+#pragma GCC optimize("O3")
+namespace FastIO {
+	inline void init() {
+		ios_base::sync_with_stdio(false);
+		cin.tie(nullptr);
+	}
+}
 
+// i =1 ∏ k pi^(ei-1) * (pi-1)
 ll phi(ll n) {
+    if (n==1) {return 1;}
+    ll ans = 1;
+    for (ll i=2; i<=n/i; i++) {
+        if (n%i==0) {
+            while(n%i==0) {
+                n/=i;
+                ans*=i;
+            }
+            ans/=i;
+            ans*=(i-1);
+        }
+    }
+    if (n>1) { ans *= (n-1); }
+    return ans; 
+} // Time Complexity: O(√n)
+
+signed main() {
+    FastIO::init();
+    ll n;
+    cin >> n;
+    cout << phi(n) << "\n";
+    return 0;
+}
+
+
+
+// 1st Version
+
+// ll fastPow(ll base, ll exponent) {
+//     ll ans = 1;
+//     while (exponent>0) {
+//         if (exponent&1) {
+//             ans*=base;
+//         }
+//         base*=base;
+//         exponent>>=1;
+//     }
+//     return ans;
+// }
+// vector<pair<ll, ll>> primeFactorization(ll n) {
+//     vector<pair<ll, ll>> ans;
+//     for (ll i=2; i<=n/i; i++) {
+//         if (n%i==0) {
+//             ll count = 0;
+//             while(n%i==0) {
+//                 n/=i;
+//                 count++;
+//             }
+//             ans.pb({i, count});
+//         }
+//     }
+//     if (n>1) { ans.pb({n,1}); }
+//     return ans;
+// }
+
+// bool isPrime(ll n) {
+//     for (ll i = 2; i<=n/i; i++) {
+//         if (n%i==0) {return false;}
+//     }    
+//     return true;
+// }
+
+// ll phi(ll n) {
+//     if (n==1) {return 1;}
+//     if (isPrime(n)) { return n-1; }
+//     vector<pair<ll, ll>> rep = primeFactorization(n);
+//     ll ans = 1;
+//     for (auto& [ba, ex] : rep) {
+//         ans = ans*fastPow(ba, ex-1)*(ba-1);
+//     }
+//     return ans;  
     
-}
+// }
 
-int main() {
-
-}
+// signed main() {
+//     FastIO::init();
+//     ll n;
+//     cin >> n;
+//     cout << phi(n) << "\n";
+//     return 0;
+// }
