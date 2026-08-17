dfc30379d639bb88e4d36e67a5bc8ba5e51f464a
longvien <vienngoclongvn@gmail.com>
2026-08-17 23:00:37 +0200

    Bug, tsk CF1458

--- Math/LCAndTask/CF1458.cpp
+++ Math/LCAndTask/CF1458.cpp
@@ -12,27 +12,28 @@ namespace FastIO {
     }
 }
 
-ll solve() {
-
-}
-
 signed main() {
     FastIO::init();
     ll n, m;
     cin >> n >> m;
-    vector<ll> a;
+    ll a1;
+    ll ggt = 0;
+    ll a2;
     vector<ll> b;
     f(i,0,n) {
-        ll curr;
-        cin >> curr;
-        a.pb(curr);
+        if (i==0) {cin >> a1;}
+        cin >> a2;
+        ggt = gcd(a2-a1, ggt);
     }
     f(i,0,m) {
         ll curr;
         cin >> curr;
         b.pb(curr);
     }
- 
+    f(i,0,m) {
+        cout << gcd(a1+b[i], ggt);
+        if (i<m-1) {cout << " ";}
+    }
     return 0;
 }
 
