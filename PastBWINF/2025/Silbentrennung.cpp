d775743770f70de3b8df23f7829c9430fa2bfc14
longvien <vienngoclongvn@gmail.com>
2026-08-19 23:19:39 +0200

    Practice Complexity Analysis

---
+++ PastBWINF/2025/Silbentrennung.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#pragma GCC optimize("O3")
+
+namespace FastIO {
+	inline void init() {
+		ios_base::sync_with_stdio(false);
+		cin.tie(nullptr);
+	}
+}
+
+signed main() {
+    FastIO::init();
+    return 0;
+}
