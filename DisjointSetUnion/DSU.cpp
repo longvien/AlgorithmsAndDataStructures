bddc0207a57180700bfa001eff4a954eac35a1f7
longvien <vienngoclongvn@gmail.com>
2026-09-19 22:28:10 +0200

    DSU in C++

---
+++ DisjointSetUnion/DSU.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#pragma GCC optimize("O3")
+
+class DSU {
+    public:
+    vector<int> parent;
+    vector<int> rank;
+    DSU(int x) {
+        parent = vector<int>(x,0);
+        rank = vector<int>(x, 0);
+    }
+    
+    
+
+    void union() {
+
+    }
+    
+}
+
+signed main() {
+    return 0;
+}
